96ae335d803caff7306fc9ec91d162901e010a8e
renxiang-hu <49302420+renxiang-hu@users.noreply.github.com>
2022-09-21 17:17:21 +0800

    ByteBuffer使用

--- src/main/java/com/itcast/netty/C1/testByteBuffer.java
+++ src/main/java/com/itcast/netty/C1/testByteBuffer.java
@@ -5,6 +5,15 @@ import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 
+/**
+ * ByteBuffer正确的使用姿势
+ * 1.向buffer写入数据，例如调用channel.read(buffer)
+ * 2.调用flip切换至读模式
+ * 3.从buffer读取数据，例如调用buffer.get()
+ * 4.调用clear()或compact()切换至写模式
+ * 5.重复1~4步骤
+ */
+
 public class testByteBuffer {
 
     public static void main(String[] args) {
@@ -16,6 +25,7 @@ public class testByteBuffer {
             while (true){
                 //从channel读取数据，向buffer写入
                 int len = channel.read(buffer);
+                System.out.println(len);
                 if (len == -1){
                     break;
                 }
@@ -23,6 +33,7 @@ public class testByteBuffer {
                 buffer.flip();  //切换至读模式
                 while (buffer.hasRemaining()){
                     byte b = buffer.get();
+                    System.out.println((char)b);
                 }
                 buffer.clear(); //切换至写模式
             }
