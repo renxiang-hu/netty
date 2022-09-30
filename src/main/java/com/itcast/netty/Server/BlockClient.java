2b68e81edf1614dcf3543b864239f3fca801a329
胡仁祥 <18917734365@163.com>
2022-09-30 13:11:08 +0800

    NIO非阻塞模式

--- src/main/java/com/itcast/netty/Server/BlockClient.java
+++ src/main/java/com/itcast/netty/Server/BlockClient.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.channels.SocketChannel;
 
-public class Client {
+public class BlockClient {
     public static void main(String[] args) throws IOException {
         SocketChannel sc = SocketChannel.open();
         sc.connect(new InetSocketAddress("127.0.0.1",8080));
