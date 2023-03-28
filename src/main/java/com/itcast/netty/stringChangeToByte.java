32a1d75c042aca675086ee584d95532d514573e1
胡仁祥 <18917734365@163.com>
2023-03-28 14:51:47 +0800

    字符串转化成byteBuffer的三种方式

---
+++ src/main/java/com/itcast/netty/stringChangeToByte.java
@@ -0,0 +1,24 @@
+package com.itcast.netty;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+
+import static com.itcast.netty.Util.ByteBufferUtil.debugAll;
+
+/**
+ * 字符串转化成byteBuffer的三种方式
+ */
+public class stringChangeToByte {
+    public static void main(String[] args) {
+        //第一种方式
+        ByteBuffer byteBuffer = ByteBuffer.allocate(16);
+        byteBuffer.put("hello".getBytes());
+        debugAll(byteBuffer);
+        //第二种方式,转换成功就切换成读模式
+        ByteBuffer byteBuffer1 = StandardCharsets.UTF_8.encode("hello");
+        debugAll(byteBuffer1);
+        //第三种方式,转换成功就切换成读模式
+        ByteBuffer byteBuffer2 = ByteBuffer.wrap("hello".getBytes());
+        debugAll(byteBuffer2);
+    }
+}
