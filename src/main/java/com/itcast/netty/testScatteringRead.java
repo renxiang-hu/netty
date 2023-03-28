f248813ebfa30c30487af55b073a80a38724ad85
胡仁祥 <18917734365@163.com>
2023-03-28 15:37:44 +0800

    byteBuffer分散读

---
+++ src/main/java/com/itcast/netty/testScatteringRead.java
@@ -0,0 +1,29 @@
+package com.itcast.netty;
+
+import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+import static com.itcast.netty.Util.ByteBufferUtil.debugAll;
+
+/**
+ * 分散读，比如"onetwothree",分别读取one，two，three
+ */
+public class testScatteringRead {
+    public static void main(String[] args) {
+        try (FileChannel fileChannel = new FileInputStream("data.txt").getChannel()){
+            ByteBuffer byteBuffer = ByteBuffer.allocate(3);
+            ByteBuffer byteBuffer1 = ByteBuffer.allocate(3);
+            ByteBuffer byteBuffer2 = ByteBuffer.allocate(5);
+            fileChannel.read(new ByteBuffer[]{byteBuffer,byteBuffer1,byteBuffer2});
+            byteBuffer.flip();
+            byteBuffer1.flip();
+            byteBuffer2.flip();
+            debugAll(byteBuffer);
+            debugAll(byteBuffer1);
+            debugAll(byteBuffer2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
